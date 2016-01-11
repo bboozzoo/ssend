9523059e7e6bf01373ff64491b49e67ce7070052
Maciek Borzecki <maciek.borzecki@gmail.com>
2016-01-11 16:04:00 +0100

    setup: package setup script

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+def version():
+    import subprocess
+    vers = subprocess.check_output('git describe --always', shell=True)
+    return vers
+
+NAME = 'Slack message send helper'
+VERSION = version()
+
+setup(
+    name=NAME,
+    version=VERSION,
+    scripts = ['ssend', 'ssend-notify']
+)
